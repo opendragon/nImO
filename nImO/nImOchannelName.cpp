42b22fb385e4847080450906d5a1ba80f0913bdd
Norman Jaffe <turing@shaw.ca>
2020-03-04 12:28:25 -0800

    Add the new 'channel name' class and document the format of channel names.

---
+++ nImO/nImOchannelName.cpp
@@ -0,0 +1,9 @@
+//
+//  nImOchannelName.cpp
+//  nImO
+//
+//  Created by Norman Jaffe on 2020/03/04.
+//  Copyright © 2020 Norman Jaffe. All rights reserved.
+//
+
+#include "nImOchannelName.hpp"
---
+++ nImO/nImOchannelName.hpp
@@ -0,0 +1,14 @@
+//
+//  nImOchannelName.hpp
+//  nImO
+//
+//  Created by Norman Jaffe on 2020/03/04.
+//  Copyright © 2020 Norman Jaffe. All rights reserved.
+//
+
+#ifndef nImOchannelName_hpp
+#define nImOchannelName_hpp
+
+#include <stdio.h>
+
+#endif /* nImOchannelName_hpp */

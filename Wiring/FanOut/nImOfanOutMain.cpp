bcbb0c2d8409cae9a2e045bf692078a72ff629eb
Norman Jaffe <turing@shaw.ca>
2024-11-04 07:15:52 -0800

    Corrected some comments.

--- Wiring/FanOut/nImOfanOutMain.cpp
+++ Wiring/FanOut/nImOfanOutMain.cpp
@@ -8,7 +8,7 @@
 //
 //  Written by: Norman Jaffe
 //
-//  Copyright:  (c) 2023 by OpenDragon.
+//  Copyright:  (c) 2024 by OpenDragon.
 //
 //              All rights reserved. Redistribution and use in source and binary forms, with or
 //              without modification, are permitted provided that the following conditions are met:
@@ -32,7 +32,7 @@
 //              ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
 //              DAMAGE.
 //
-//  Created:    2023-01-29
+//  Created:    2024-11-04
 //
 //--------------------------------------------------------------------------------------------------
 

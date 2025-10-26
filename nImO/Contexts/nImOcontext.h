4194db9c5bb21988efcbd59c2ab2f98131647e08
Norman Jaffe <turing@shaw.ca>
2025-10-26 11:03:06 -0700

    The typedef mapping 'io_service' objects to 'io_context' is not always present in the Asio header files.

--- nImO/Contexts/nImOcontext.h
+++ nImO/Contexts/nImOcontext.h
@@ -73,6 +73,9 @@ namespace nImO
     class ServiceContext;
     class UtilityContext;
 
+    /*! @brief A holder for a shared pointer to an Asio context (service). */
+    using SPservice = std::shared_ptr<boost::asio::io_context>;
+
     /*! @brief A holder for a shared pointer to an InputOutputContext. */
     using SpInputOutputContext = std::shared_ptr<InputOutputContext>;
 
@@ -82,11 +85,8 @@ namespace nImO
     /*! @brief A holder for a shared pointer to a UtilityContext. */
     using SpUtilityContext = std::shared_ptr<UtilityContext>;
 
-    /*! @brief A holder for a shared pointer to an Asio service . */
-    using SPservice = std::shared_ptr<boost::asio::io_service>;
-
     /*! @brief A holder for a unique pointer to an Asio 'work' placeholder. */
-    using UPwork = std::unique_ptr<boost::asio::io_service::work>;
+    using UPwork = std::unique_ptr<boost::asio::io_context::work>;
 
     /*! @brief A class to provide binary data with unknown structure. */
     class Context
